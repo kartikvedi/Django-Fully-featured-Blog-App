cf6a03582742640ff5bb1f7656303134103d0828
Kartik Vedi <kartikvedi15@gmail.com>
2022-03-20 16:10:00 +0530

    Added Allowed Host

--- django_project/settings.py
+++ django_project/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['kartikblogapp.herokuapp.com']
+ALLOWED_HOSTS = ['djangoblogappbykv.herokuapp.com']
 
 
 # Application definition
